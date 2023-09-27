77c315d084a70101f91fce8fd9fe83a710d53a81
waqas2714 <waqasali00123@gmail.com>
2023-09-27 21:36:23 +0500

    Layouts

---
+++ frontend/src/app/about/layout.js
@@ -0,0 +1,11 @@
+
+const Layout = ({children}) => {
+    return (
+      <div>
+          <h3>Common layout for about pages.</h3>
+          {children}
+      </div>
+    )
+  }
+  
+  export default Layout
