cc3e7ec67d927f31aa3ad37842a4e0c49f7f9c9a
waqas2714 <waqasali00123@gmail.com>
2023-09-27 17:00:04 +0500

    Fuctions, Inner components and states

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,21 +1,26 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+  const [nameList, setNameList] = useState(["Waqas", "Maaz"]);
+  const [name, setName] = useState(nameList[0]);
+  const [nameBool, setNameBool] = useState(true);
+  const changeName = ()=>{
+    nameBool ? setName(nameList[0]) : setName(nameList[1]);
+    setNameBool(!nameBool);
+  }
+
+  const Username = ()=>{
+    return(
+      <h1>User name is {name}</h1>
+    )
+  }
   return (
     <main className={styles.main}>
-      <h1>Hello World</h1>
-      <User userName={"Waqas"}/>
-      <User userName={"Numair"}/>
-      <User userName={"Maaz"}/>
-      <User userName={"Safwan"}/>
-      <User userName={"Ali"}/>
+    <Username />
+    <button onClick={changeName}>Click Me</button>
     </main>
-  )
-}
-
-const User = ({userName})=>{
-  return(
-    <h1>User name is {userName}</h1>
   )
 }
